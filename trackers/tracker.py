6e4416d5df922b4d7f0f1f890c763999d6a2b9b4
Anil Thapa <anilthapa1.at23@gmail.com>
2025-02-03 23:47:17 -0600

    Updated tracker code

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -8,22 +8,32 @@ class Tracker:
         self.tracker = sv.ByteTrack()
     
     def detect_frames(self, frames):
-        batch_size = 16
+        batch_size = 6
         detections = []
         for i in range(0, len(frames), batch_size):
-            detections_batch = self.model.predict(frames[i:i+batch_size], conf=0.25)
-            detections.append(detections_batch)
+            detection = self.model.predict(frames[i: i+batch_size], conf = 0.25)
+            detections.append(detection)
             break
         return detections
-        
+
     def get_object_track(self, frames):
         detections = self.detect_frames(frames)
-        for frame_num, detection in enumerate(detections):
-            class_names = detection[0].names
-            class_names_inv = {v:k for k, v in class_names.items()}
-            print(detection)
-            try:
-                detection_supervision = sv.Detections.from_ultralytics(detection)
-                print(detection_supervision)
-            except Exception as e:
-                print('Error Processing at ', e)
+        
+        for detection in detections:
+            
+            class_names = detection.names
+            class_names_inv = {v:k for v, k in class_names.items()}
+            
+            # Convert into supervision detection format
+            detection_sv = sv.Detections.from_ultralytics(detection[0])
+
+            # Track Objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_sv)
+            
+            # Convert Goalkeeper into player
+            for object_id, class_id in enumerate(detection_with_tracks.class_id):
+                if class_names[class_id] == 'goalkeeper':
+                    detection_sv.class_id[object_id] = class_names_inv['player']
+             
+            
+            
